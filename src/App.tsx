3addd73580199de1bbfff40ead3804881e0a0c23
teebin2019 <teebin2019@gmail.com>
2026-03-06 11:40:41 +0700

    update Create

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { NavLink } from "react-router";
 
 interface Food {
   id: number;
@@ -25,60 +26,76 @@ function App() {
 
   return (
     <>
-      <h1 className="text-5xl font-bold mb-5">Food App</h1>
-
-      <div className="relative overflow-x-auto">
-        <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-          <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-            <tr>
-              <th scope="col" className="px-6 py-3">
-                ลำดับ
-              </th>
-              <th scope="col" className="px-6 py-3">
-                ชื่ออาหาร
-              </th>
-              <th scope="col" className="px-6 py-3">
-                ราคา
-              </th>
-              <th scope="col" className="px-6 py-3">
-                ดำเนินการ
-              </th>
-            </tr>
-          </thead>
-          <tbody>
-            {data.map((item, idx) => (
-              <tr
-                className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 border-gray-200"
-                key={item.id}
-              >
-                <th
-                  scope="row"
-                  className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-                >
-                  {idx + 1}
-                </th>
-                <td className="px-6 py-4">{item.name}</td>
-                <td className="px-6 py-4">{item.price}</td>
-                <td className="px-6 py-4">
-                  <button
-                    type="button"
-                    onClick={() => alert("แก้ไข")}
-                    className="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-                  >
-                    แก้ไข
-                  </button>
-                  <button
-                    type="button"
-                    onClick={() => alert("ลบ")}
-                    className="text-white bg-red-700 hover:bg-red-800 focus:outline-none focus:ring-4 focus:ring-red-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900"
-                  >
-                    ลบ
-                  </button>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
+      <div className="container mx-auto">
+        <div className="space-y-10">
+          <div>
+            <h1 className="text-5xl font-bold text-center">Food App</h1>
+          </div>
+          <div>
+            <NavLink
+              to="/add"
+              className="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              end
+            >
+              เพิ่มอาหาร
+            </NavLink>
+          </div>
+          <div>
+            <div className="relative overflow-x-auto">
+              <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                  <tr>
+                    <th scope="col" className="px-6 py-3">
+                      ลำดับ
+                    </th>
+                    <th scope="col" className="px-6 py-3">
+                      ชื่ออาหาร
+                    </th>
+                    <th scope="col" className="px-6 py-3">
+                      ราคา
+                    </th>
+                    <th scope="col" className="px-6 py-3">
+                      ดำเนินการ
+                    </th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {data.map((item, idx) => (
+                    <tr
+                      className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 border-gray-200"
+                      key={item.id}
+                    >
+                      <th
+                        scope="row"
+                        className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                      >
+                        {idx + 1}
+                      </th>
+                      <td className="px-6 py-4">{item.name}</td>
+                      <td className="px-6 py-4">{item.price}</td>
+                      <td className="px-6 py-4">
+                        <button
+                          type="button"
+                          onClick={() => alert("แก้ไข")}
+                          className="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                        >
+                          แก้ไข
+                        </button>
+                        <button
+                          type="button"
+                          onClick={() => alert("ลบ")}
+                          className="text-white bg-red-700 hover:bg-red-800 focus:outline-none focus:ring-4 focus:ring-red-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900"
+                        >
+                          ลบ
+                        </button>
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
---
+++ src/Create.tsx
@@ -0,0 +1,55 @@
+function Create() {
+  return (
+    <>
+      <div className="container mx-auto">
+        <div className="space-y-10">
+          <div>
+            <h1 className="text-5xl font-bold text-center">Food App</h1>
+          </div>
+          <div>
+            <form className="max-w-full mx-auto">
+              <div className="mb-5">
+                <label
+                  htmlFor="text"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >
+                  ชื่ออาหาร
+                </label>
+                <input
+                  type="text"
+                  id="name"
+                  className="shadow-xs bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-xs-light"
+                  placeholder="กรุณากรอกชื่ออาหาร"
+                  required
+                />
+              </div>
+              <div className="mb-5">
+                <label
+                  htmlFor="price"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >
+                  ราคา
+                </label>
+                <input
+                  type="number"
+                  id="price"
+                  className="shadow-xs bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-xs-light"
+                  placeholder="กรุณากรอกราคาอาหาร"
+                  required
+                />
+              </div>
+              <button
+                type="submit"
+                className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              >
+                บันทึก
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Create;
